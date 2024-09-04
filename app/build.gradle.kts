bb40e3d1a1670fdcd96b0c04a098adcca0c90646
sophiekumagai <sophie.satie20@gmail.com>
2024-09-04 11:14:40 -0300

    adicionando as dependências e a tela de escolha do usuário

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,19 +42,44 @@ android {
 
 dependencies {
 
+    // Firebase BoM (Bill of Materials) para gerenciar versões de dependências do Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+
+    // Glide para carregamento e manipulação eficiente de imagens
     implementation("com.github.bumptech.glide:glide:4.14.2")
+
+    // AppCompat para compatibilidade com versões antigas do Android
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    // Material Components para utilizar os componentes de design do Google Material
     implementation("com.google.android.material:material:1.12.0")
+
+    // ConstraintLayout para criar layouts com restrições flexíveis e dinâmicas
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Lottie para renderizar animações no formato JSON do Adobe After Effects
     implementation("com.airbnb.android:lottie:6.3.0")
-    implementation ("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("com.google.android.material:material:1.12.0")
+
+    // ViewPager2 para criar navegação swipeable entre fragmentos
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.0")
+
+    // JUnit para testes unitários
     testImplementation("junit:junit:4.13.2")
+
+    // Extensão do JUnit para testes de UI no Android
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+
+    // Espresso para automação de testes de UI no Android
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation ("com.google.android.material:material:1.9.0")
+
+    // CircleImageView para criar imagens de perfil circulares
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    // Splash Screen API para a tela de splash nas versões recentes do Android
     implementation("androidx.core:core-splashscreen:1.0.0")
+
 
 }
