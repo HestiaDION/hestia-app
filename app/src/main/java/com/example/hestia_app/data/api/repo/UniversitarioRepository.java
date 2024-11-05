875e2373e8ad72886c37c6463db9f995cdcd6305
laura4343 <123laurafarias@gmail.com>
2024-11-05 08:29:32 -0300

    a

--- app/src/main/java/com/example/hestia_app/data/api/repo/UniversitarioRepository.java
+++ app/src/main/java/com/example/hestia_app/data/api/repo/UniversitarioRepository.java
@@ -5,6 +5,7 @@ import com.example.hestia_app.domain.models.Universitario;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.Call;
+import retrofit2.http.Header;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
